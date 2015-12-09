93f02a2fa3200bc2781d122b61f5a718ec0ddd43
Eli Perelman <eli@eliperelman.com>
2015-12-09 09:33:47 -0500

    Initial commit, migration from @mozilla/raptor

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let submit = require('./lib');
+let R = require('ramda');
+
+module.exports = (cli) => {
+  let command = cli.command('submit <metrics>');
+
+  command
+    .description('Submit a performance metrics file to an InfluxDB database')
+    .action((args) => {
+      return Promise
+        .resolve(R.merge({ metrics: metrics }, args.options))
+        .then(submit)
+        .then(() => cli.log('Database results submitted successfully'))
+        .catch(cli.exits)
+    });
+
+  cli.usesDatabase(command);
+
+  return command;
+};
